ed454bb35139adc7a172c8b7cc5710fa364d6b0f
laon <cj4luv@gmail.com>
2020-11-27 09:25:00 +0900

    404페이지 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,52 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
-import Home from '@views/Home.vue';
 import { routerPaths } from '@constants';
+import { Home } from '@views';
+import { DefaultLayout } from '@layouts';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: routerPaths.root,
-    name: 'Home',
-    component: Home,
+    name: 'root',
+    component: DefaultLayout,
+    children: [
+      {
+        path: routerPaths.home,
+        name: 'Home',
+        meta: { requiresAuth: true },
+        component: Home,
+      },
+    ],
   },
   {
-    path: '/about',
-    name: 'About',
+    path: routerPaths.login,
+    name: 'Login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '@layouts/LoginLayout.vue'),
+    meta: { requiresAuth: false },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    const auth = true;
+    if (!auth) {
+      next({
+        path: routerPaths.login,
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ src/views/index.ts
@@ -0,0 +1,2 @@
+export { default as Home } from './Home.vue';
+export { default as NotFound } from './NotFound.vue';
