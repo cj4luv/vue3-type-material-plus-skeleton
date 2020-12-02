6ffddae4b08f1ac270b6372bf02725ab11b1119b
laon <cj4luv@gmail.com>
2020-12-02 14:59:16 +0900

    modify store index ts file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,32 +1,45 @@
 /* eslint-disable no-param-reassign */
 import { createStore } from 'vuex';
 import getters from './getters';
+import { state as appState } from './modules/app';
 
 // https://webpack.js.org/guides/dependency-management/#requirecontext
 const modulesFiles = require.context('./modules', true, /\.ts$/);
 
+export const State = {
+  app: appState,
+};
+
+export type State = typeof State;
+
+export interface Ac {
+  [key: string]: State | any;
+}
+
 // you do not need `import app from './modules/app'`
 // it will auto require all vuex module from modules file
-const modules: any = modulesFiles.keys().reduce((ac: any, modulePath: string) => {
+const modules = modulesFiles
+  .keys().reduce((ac: Ac, modulePath) => {
   // set './app.js' => 'app'
-  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1');
-  const value = modulesFiles(modulePath);
-  // console.log('moduleName', m, moduleName, value);
+    const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1');
+    const value = modulesFiles(modulePath);
+    console.log('moduleName', value.default, moduleName);
 
-  if (ac && ac[moduleName]) {
-    ac = {
-      [value.default]: {},
-    };
-  } else {
-    ac[moduleName] = {
-      ...ac,
-      ...value.default,
-    };
-  }
+    if (ac && ac[moduleName]) {
+      ac = {
+        ...ac,
+        [value.default]: {},
+      };
+    } else {
+      ac[moduleName] = {
+        ...ac,
+        ...value.default,
+      };
+    }
 
-  console.log('ac', ac);
-  return ac;
-}, {});
+    console.log('ac', ac);
+    return ac;
+  }, {});
 
 export default createStore({
   // state: {
